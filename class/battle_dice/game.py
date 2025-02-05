593d12bf5dbefc416635cfa12972471e2f7235e8
pmkunz <pmkunz@willamette.edu>
2025-02-05 11:15:47 -0800

    Wednesday 2/5 update

--- class/battle_dice/game.py
+++ class/battle_dice/game.py
@@ -1,6 +1,7 @@
 
 import random
 from character import Character
+# from charactertype import CharacterType
 
 class Game:
     """Manages the Dice Battle game logic."""
@@ -12,22 +13,29 @@ class Game:
 
     def attack(self, attacker: Character, defender: Character):
         """Performs an attack where the attacker rolls a die to determine damage dealt."""
-        die_roll = random.randint(1, 6)
+        die_roll = random.randint(1, 6)     # Random number between 1 and 6
 
-        #...
+        defender.health -= die_roll * attacker.attack_power
+        print(f"{attacker.name} attacks {defender.name} and does {die_roll} damage.")
+        print(f"{defender.name} now has {max(0, defender.health)} health remaining. \n")
 
     def start_battle(self):
         """Starts a turn-based battle between two players."""
+        
+        print(f"Battle Start: {self.__player1.name} vs {self.__player2.name}\n")
+
         turn = 1
 
-        if turn % 2 != 0:
-            attacking_player = self.player1
-            defending_player = self.player2
-        else:
-            attacking_player = self.player2
-            defending_player = self.player1
+        while self.__player1.health > 0 and self.__player2.health > 0:
+            if turn % 2 != 0:                           # Odd-numbered turns
+                attacking_player = self.player1
+                defending_player = self.player2
+            else:                                       # Even-numbered turns
+                attacking_player = self.player2
+                defending_player = self.player1
 
-        #...
+            # Switch turns
+            turn += 1
 
-        # Switch turns
-        turn += 1
+        winner: Character = self.__player1 if self.__player1.health > 0 else self.__player2
+        print(f"{winner.name} wins the battle!")
--- class/battle_dice/main.py
+++ class/battle_dice/main.py
@@ -3,10 +3,16 @@ from game import Game
 from character import Character
 from charactertype import CharacterType
 
-# Create characters
-alice = Character(name="Alice", character_type=CharacterType.WARRIOR, health=100, attack_power=10)
-bob = Character(name="Bob", character_type=CharacterType.MAGE, health=70, attack_power=8)
 
-# Start the game
-game = Game(alice, bob)
-game.start_battle()
+def main():
+
+    # Create characters
+    alice = Character(name="Alice", character_type=CharacterType.WARRIOR, health=100, attack_power=10)
+    bob = Character(name="Bob", character_type=CharacterType.MAGE, health=70, attack_power=8)
+
+    # Start the game
+    game = Game(alice, bob)
+    game.start_battle()
+
+if __name__ == "__main__":
+    main()
